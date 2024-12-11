1e056fee69a036989e76df42591acb76c5a9daec
Mohammad Tasin <102211311+Mrtasin@users.noreply.github.com>
2024-12-11 21:22:12 +0530

    Add files via upload

---
+++ Code-File/Operator.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()  {
+    // int x = 10, z;
+    // z = x--;
+    // cout<<z<<" "<<x;
+    // float f;
+    // cout<<sizeof(double);
+    // cout<<sizeof(f);
+    // cout<<sizeof(10);
+    // char ch = 'T';
+    // cout<<sizeof('T');
+    // cout<<sizeof(ch);
+    // cout<<sizeof(10.0F);
+    
+    // cout<<10+20<<endl;
+    // cout<<10/3<<endl;
+    // cout<<10/3.0F<<endl;
+
+    cout<<10%3;
+
+    cout<<endl;
+    return 0;
+}
