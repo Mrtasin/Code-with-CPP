0fea6aaa3070b2fcf2b38e50b1fc805655c02cbb
Mohammad Tasin <102211311+Mrtasin@users.noreply.github.com>
2024-12-11 17:18:33 +0000

    operator2

--- Code-File/operator2.cpp
+++ Code-File/operator2.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main()  {
-    
+
+    // cout<<(10^5)<<endl;
+    // cout<<(~-10)<<endl;
+    // cout<<(20<<3)<<endl;
+    // cout<<(10<20)<<endl;
+    // cout<<(20>30)<<endl;
+    // cout<<(20!=3)<<endl;
+    // int x = 10;
+    cout<<(!0)<<endl;
+    cout<<endl;
+    return 0;
 }
