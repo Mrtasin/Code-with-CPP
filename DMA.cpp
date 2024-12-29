2739703666834267b5db65a5bd0a6aa3c204ce05
Mohammad Tasin <102211311+Mrtasin@users.noreply.github.com>
2024-12-29 21:58:38 +0530

    Add files via upload

---
+++ DMA.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()  {
+    int *ptr = new int;
+    *ptr = 10;
+    int *arr = new int[10];
+    5[arr] = 50;
+
+    delete []arr;
+
+    delete ptr;
+
+    cout<<"Data is "<<arr[5];
+    
+    cout<<endl;
+    return 0;
+}
