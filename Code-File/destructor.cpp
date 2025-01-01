e882b9d115470b7c81b479813ca23c1ccc6146a7
Mohammad Tasin <102211311+Mrtasin@users.noreply.github.com>
2025-01-01 16:51:58 +0530

    Add files via upload

---
+++ Code-File/destructor.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class Item  {
+    private:
+        int num;
+        int *ptr;
+    public:
+        Item()  {
+            ptr = new int;
+        }
+        Item(int a, int b)  {
+            ptr = new int;
+            num = a; *ptr = b;
+        }
+        ~Item()  {
+            delete ptr;
+        }
+        void setptr(int data)   {
+            *ptr = data;
+        }
+        int getptr()    {
+            return *ptr;
+        }
+
+        void setNum(int num)    {
+            this->num = num;
+        }
+        int getNum()    {
+            return num;
+        }
+
+        void del()  {
+            delete ptr;
+        }
+};
+
+int main()  {
+    Item i2(10,20),i1;
+    Item obj = i2;
+    i2.del();
+    cout<<obj.getptr()<<" "<<obj.getNum()<<endl;
+
+    cout<<endl;
+    return 0;
+}
